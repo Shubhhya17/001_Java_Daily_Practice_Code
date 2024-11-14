5df65ae376f321e58cade4aec67f3438839f28f5
Shubham Pawar <shubhpawar1703@gmail.com>
2024-11-14 16:13:52 +0530

    StringEx

--- StringRev2.java
+++ StringRev2.java
@@ -1,10 +1,19 @@
 public class StringRev2 {
     public static void main(String args[]){
-        String s = "Shubham";
-        String rev = " ";
+        String s = "maswam";
+        String rev = "";
         for(int i = s.length()-1; i>=0; i-- ){
             rev +=s.charAt(i);
+            System.out.println(rev);
         }
+        if(s.equals(rev)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+        
         System.out.println(rev);
+
+
     }
 }
