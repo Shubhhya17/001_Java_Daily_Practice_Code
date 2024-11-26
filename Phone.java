5c4d7b68d1afcca431a055f5071a276cd2f79f5d
Shubham Pawar <shubhpawar1703@gmail.com>
2024-11-26 13:08:36 +0530

    java code

---
+++ Phone.java
@@ -0,0 +1,24 @@
+abstract class Realme{
+    void playYoutube(){
+        System.out.println("Playing Youtube form Realme");
+    }
+    abstract void sendSMS();
+}
+class Airtel extends Realme{
+    void sendSMS(){
+        System.out.println("Sending SMS form Airtel");
+    }
+}
+class Jio extends Realme{
+    void sendSMS(){
+        System.out.println("Sending sms from jio");
+    }
+}
+
+class Phone {
+    public static void main(String args[]){
+        Realme r1 = new Airtel();
+        r1.playYoutube();
+        r1.sendSMS();
+    }
+}
