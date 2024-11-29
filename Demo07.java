2f8cbef1cfcdab9c735dd4e20913af63cd8ea3ce
Shubham Pawar <shubhpawar1703@gmail.com>
2024-11-29 23:13:15 +0530

    Modified in class

--- Demo07.java
+++ Demo07.java
@@ -4,13 +4,18 @@ class A{
     }
 }
 
-class Demo07 {
+class Demo07 extends A {
     void m1(int a, int b){
         System.out.println("B Demo");
         
     }
     public static void main(String args[]){
-        Demo07 d1 = new Demo07();
-        d1.m1(1, 2);
+       //Demo Prints
+        // Demo07 d1 = new Demo07();
+        // d1.m1(1, 2);
+
+        //Class A 
+        // A a = new A();
+        // a.m1(1,2);
     }
 }
