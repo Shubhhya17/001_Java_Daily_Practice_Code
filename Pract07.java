d84922dd9c34f8cd455be0300af2b94c5e5be9c3
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-09 20:42:01 +0530

    Try_Finally_Finally

---
+++ Pract07.java
@@ -0,0 +1,18 @@
+
+public class Pract07 {
+
+    public static void main(String args[]){
+        try{
+            System.out.println("Java");
+        }
+        finally{
+            System.out.println("Python");
+        }
+        try{   
+            System.out.println("Web Developer");
+        }
+        finally{
+            System.out.println("JavaScript");
+        }
+    }
+}
