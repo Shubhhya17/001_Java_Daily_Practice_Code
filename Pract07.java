4ebd7476f35c016f7fa2e8a7df56297ccd7c7edd
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-09 20:45:15 +0530

    Try_Finnaly

--- Pract07.java
+++ Pract07.java
@@ -2,17 +2,23 @@
 public class Pract07 {
 
     public static void main(String args[]){
+        // try{
+        //     System.out.println("Java");
+        // }
+        // finally{
+        //     System.out.println("Python");
+        // }
+        // try{   
+        //     System.out.println("Web Developer");
+        // }
+        // finally{
+        //     System.out.println("JavaScript");
+        // }
+
         try{
-            System.out.println("Java");
-        }
-        finally{
-            System.out.println("Python");
-        }
-        try{   
-            System.out.println("Web Developer");
-        }
-        finally{
-            System.out.println("JavaScript");
+            System.out.println("1 try without catch and finally ");
+        }finally{
+            System.out.println("1 finally block is also there");
         }
     }
 }
