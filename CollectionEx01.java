82e131639a2d90c1e1beae2f60139d13e4e2063b
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-10 11:49:11 +0530

    Collection_Framework

---
+++ CollectionEx01.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class CollectionEx01 {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+
+        System.out.println(list);
+}
+}
---
+++ CollectionEx02.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CollectionEx02 {
+    public static void main(String args[]){
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        System.out.println(al); 
+    }
+}
