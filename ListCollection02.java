4ca9610cf91d353ae62d7bac43193858cec24650
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-12 22:13:26 +0530

    ListCollection

--- ListCollection02.java
+++ ListCollection02.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class ListCollection02 {
     public static void main(String[] args) {
         ArrayList<Object> al = new ArrayList<>();
-        al.add(true);
         al.add(123);
+        al.add(true);
+        al.add(true);
+        al.add(true);
         al.add("Shubh");
         al.add("Pawar");
-        al.add(true);
-        al.add(true);
         System.out.print(al);
     }
 }
