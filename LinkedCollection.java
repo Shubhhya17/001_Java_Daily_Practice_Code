0c91caba14c8df1f0d04e5ee3a5eeff40d587632
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-12 22:33:02 +0530

    LinkedList

---
+++ LinkedCollection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LinkedCollection {
+    @SuppressWarnings("unchecked")
+    public static void main(String args[]){
+        LinkedList l1 = new LinkedList();
+        l1.add(12);
+        l1.add(22);
+        l1.add("Shubh");
+        l1.add(true);
+        System.out.println(l1);
+    }
+}
+
+
