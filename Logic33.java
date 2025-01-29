cddfe2beda30c3b863ff3c746bf3aad6eb77e85b
Shubham Pawar <shubhpawar1703@gmail.com>
2025-01-29 15:30:35 +0530

    Java

--- Logic33.java
+++ Logic33.java
@@ -1,9 +1,29 @@
+// public class Logic33 {
+//     public static void main(String args[]) {
+//         int arr[] = {1, 2, 4, 4, 5, 6}; // Input array
+//         int min = arr[0]; // Assume the first element is the minimum
 
-// 15.Write a java program to print minimum number of an array.
-//  I/p int[] a={1,4,2,5};
+//         // Loop through the array to find the minimum
+//         for (int i = 1; i < arr.length; i++) {
+//             if (arr[i] < min) {
+//                 min = arr[i]; // Update min if a smaller element is found
+//             }
+//         }
 
-public class Logic33 {
+//         System.out.println("Minimum number: " + min);
+//     }
+// }
+
+class Logic33{
     public static void main(String args[]){
-        
+        int arr[] = {43,23,54,43};
+        int min = arr[0];
+
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i]> min){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
     }
 }
