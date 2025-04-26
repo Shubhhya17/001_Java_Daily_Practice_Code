6d350f936170f230143b6395bd51305956427126
Shubhhya17 <shubhpawar1703@gmail.com>
2025-04-26 23:37:16 +0530

    changes

--- Collector2.java
+++ Collector2.java
@@ -3,10 +3,10 @@ import java.util.stream.Collectors;
 public class Collector2 {
     public static void main(String args[]){
         List<String> names = Arrays.asList("john", "jane", "tom");
-        List<String> names = Arrays.asList("john", "jane", "tom");
+        List<String> names2 = Arrays.asList("john", "jane", "tom");
 
 List <String> upperCaseNames = names.stream().map(String::toUpperCase).collect(Collectors.toList());
-List <String> upperCaseNames = names.stream().map(String::lowerCaseCase).collect(Collectors.toList());
+List <String> upperCaseName = names.stream().map(String::lowerCaseCase).collect(Collectors.toList());
 
 System.out.println(upperCaseNames);
     }
