eb6b8079cca6d9b051add27c3c5405cc83915266
Shubhhya17 <shubhpawar1703@gmail.com>
2025-04-30 00:58:23 +0530

    practice

---
+++ Practice07.java
@@ -0,0 +1,18 @@
+class Practice07{
+    public static void main(String args[]){
+        int a = 10 ;
+        int b = 20 ;
+        int c = 30;
+
+        if(a> b && a>c){
+            System.out.println( a +" is the greater value");
+        }
+        else if (b> c) {
+            System.out.println(b + " is the greater value");
+        }
+        else{
+            System.out.println(c + " is the greater value");
+
+        }
+    }
+}
